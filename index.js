4d220e180e0ac874dcc5a5287ce6d8cbb157d6ff
uyransilva <uyran.silva@hotmail.com>
2022-11-17 10:47:46 -0300

    Morning 11-17 commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const app = express()
+const bodyParser = require('body-parser');//disponibiliza para o backend o objeto body
+const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
-
+app.use(bodyParser.urlencoded({extended:false}));   //recebe os dados do form e transforma em estrutura JS para uso
+app.use(bodyParser.json()); //Permite a leitura de dados de form via JSON
 
 app.get('/', ( req , res ) => {
     
@@ -13,6 +14,14 @@ app.get('/', ( req , res ) => {
 
 app.get('/perguntar',( req, res ) => {
     res.render("perguntar");
+})
+
+app.post('/salvarpergunta', ( req , res ) => {
+    //recebe os dados do formulario a partir do "name" do campo
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+
+    res.send(`Titulo ${titulo} e descrição:${descricao}`)
 })
 
 app.listen(3000, () => {
